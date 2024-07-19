a07f8ac099ec5b2df36a5267b0d8939a87b3835a
andrewdo <andrewdo@gmail.com>
2024-07-19 20:36:48 +0300

    modify styling

--- src/components/Collections/CollectionPage.jsx
+++ src/components/Collections/CollectionPage.jsx
@@ -10,7 +10,7 @@ export default function CollectionPage() {
 <div className="p-5 mt-10 mx-auto sm:p-10 md:p-16 dark:text-gray-800 container">
 	<div className="flex flex-col max-w-3xl mx-auto overflow-hidden rounded">
 		<div alt="" className="w-full h-60 sm:h-96 dark:bg-gray-500" style={{backgroundImage:`url(${target.imgsLink.type})`,backgroundPosition:"center center",backgroundSize:"cover"}}></div>
-		<div className="p-6 pb-12 m-4 mx-auto -mt-16 space-y-6 lg:max-w-2xl sm:px-10 sm:mx-12 lg:rounded-md dark:bg-gray-100">
+		<div className="p-6 pb-12 m-4 mx-auto -mt-16 space-y-6 lg:max-w-2xl sm:px-10 sm:mx-12 lg:rounded-md bg-gray-100">
 			<div className="space-y-2">
 				<div rel="noopener noreferrer" href="#" className="inline-block text-2xl font-semibold sm:text-3xl capitalize">{target.title}</div>
 				<p className="text-xs dark:text-gray-600">By  
