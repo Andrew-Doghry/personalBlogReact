1cc88971512ceafc45affbddb2f6ece17a286996
andrewdo <andrewdo@gmail.com>
2024-08-22 15:27:38 +0300

    fix eslint problem

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,9 +1,7 @@
-import { Outlet, useRouteLoaderData, useRoutes } from 'react-router-dom'
+import { Outlet } from 'react-router-dom'
 import Header from './components/Header/Header'
 import Footer from './components/Footer/Footer'
-import { motion, useScroll } from "framer-motion"
 function Layout() {
-  const { scrollYProgress } = useScroll()
 
   return (
 
